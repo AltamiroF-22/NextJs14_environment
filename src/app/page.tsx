cfe7848b244a2731bef5c628e4f61f4c5a4b399b
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-05-02 15:25:25 -0300

    image added on background just for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-sm bg-blue-900 w-screen text-center p-2">
+    <main className="flex min-h-screen flex-col items-center p-24 bg-[url('../assets/img/pexels-pixabay-301614.jpg')] bg-center bg-cover">
+      <h1 className="text-lg text-center p-2">
         Environment
       </h1>
-      <article className="flex gap-2 w-screen justify-center bg-slate-400 p-2">
-        <p>Next.js 14</p>
+      <article className="flex gap-2 w-screen justify-center p-2">
+        <p>Next14</p>
         <p>TailwindCss</p>
         <p>Jest</p>
       </article>
